825b25dd6008bb6be2f9fd246fc6ef5b10f76378
Joey Lansangan <joeylansangan@gmail.com>
2019-06-04 13:50:40 -0500

    added value for 1st item

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,15 @@ $("#number-to-reach").append(targetNumber);
 
 // set initial user score to 0
 var profit = 0;
+// show profit to its own div
+$("#profit").append(profit);
 
 
 // // set value to each item
 var randomPrice = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
 var itemPrice = [randomPrice];
-console.log(itemPrice);
+
 for (let i = 0; i < itemPrice.length; i++){
     
     // make image for itemOne
@@ -31,8 +33,23 @@ for (let i = 0; i < itemPrice.length; i++){
     
 }
 
-// show items to its own div
 // set on click event for each item
+$(".item-image").on("click", function(){
+
+    // create variable that has a value of the item price which is under the data-itemvalue attribute
+    var itemValue = ($(this).attr("data-itemvalue"));
+
+    // itemValue will return a string value, parseInt will convert this string value to integer
+    itemValue = parseInt(itemValue);
+
+    // add itemValue to profit everyclick
+    // can also be typed as profit += itemValue
+    // profit = profit + itemValue
+    
+})
+
+// show items to its own div
+
 
 // add score depending on the value of item clicked
 // add if statement for win/lose
