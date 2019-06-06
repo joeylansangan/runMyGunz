6f0393b4a9701df7a17eee16dbadcb4ecd99c423
Joey Lansangan <joeylansangan@gmail.com>
2019-06-05 21:13:43 -0500

    organized js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,8 @@
-
-
-// Set target number randomly from 19 - 120
-var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-
 // show target number to its own div
 $("#number-to-reach").append(targetNumber);
 
-// set initial user score to 0
-var profit = 0;
 // show profit to its own div
 $("#profit").append(profit);
-
-
-// // set value to each item
-var randomPrice = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var randomPrice2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var randomPrice3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-var randomPrice4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-
-var itemPrice = [randomPrice, randomPrice2, randomPrice3, randomPrice4];
-console.log(itemPrice);
 
 function displayGuns() {
 
@@ -30,7 +13,7 @@ for (let i = 0; i < itemPrice.length; i++){
     // add class for each item
     itemOne.addClass("item-image");
     // add image attribute on the item
-    // template literals was used here
+    // template literals was used here to minimize code
     itemOne.attr("src", `./assets/images/${i}.png`);
     // add random item value
     itemOne.attr("data-itemvalue", itemPrice[i]);
@@ -40,13 +23,6 @@ for (let i = 0; i < itemPrice.length; i++){
 
 }
 }
-displayGuns();
-
-// create win/loss counter and show initial value on screen
-var win = 0
-$("#wins").append(win);
-var loss = 0
-$("#loss").append(loss); 
 
 function resetStats(){
     // reset targetNumber
@@ -59,21 +35,20 @@ function resetStats(){
     $("#profit").html(profit);
 
     // get 4 new values for each gun
- randomPrice = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
- randomPrice2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
- randomPrice3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
- randomPrice4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    randomPrice = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    randomPrice2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    randomPrice3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    randomPrice4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
- itemPrice = [randomPrice, randomPrice2, randomPrice3, randomPrice4];
+    itemPrice = [randomPrice, randomPrice2, randomPrice3, randomPrice4];
 
- displayGuns();
- 
-
- console.log(itemPrice);
+    displayGuns();
+    console.log(itemPrice);
 
 }
 
-var itemValue;
+displayGuns();
+
 // set on click event for each item
 $(document).on("click", ".item-image", function(){
     // sound effect
@@ -92,26 +67,40 @@ $(document).on("click", ".item-image", function(){
     $("#profit").text(profit);
 
     // if statement for win/lose
-    // create a win/lose count variable and show on screen
-
 
     if (profit === targetNumber){
         alert("You Win!");
         win++;
         $("#wins").html(win);
         resetStats();
-        
-        
-       
     }
     else if (profit >= targetNumber){
         alert("You Lose!"); 
         loss++;
         $("#loss").html(loss);
         resetStats();
-       
-        
-        
     }
   
 })
+
+// Set target number randomly from 19 - 120
+var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+// set initial user score to 0
+var profit = 0;
+
+// // set value to each item
+var randomPrice = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var randomPrice2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var randomPrice3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var randomPrice4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+var itemPrice = [randomPrice, randomPrice2, randomPrice3, randomPrice4];
+console.log(itemPrice);
+
+// create win/loss counter and show initial value on screen
+var win = 0
+$("#wins").append(win);
+var loss = 0
+$("#loss").append(loss); 
+
+var itemValue;
